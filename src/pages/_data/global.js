0a250d70e26c883c7b54749a7b008a88b6002a79
Dustin Whisman <whisman.dustin@gmail.com>
2022-11-20 11:13:26 -0700

    feat: provide fallback for masonry layout (#20)
    * chore: install image-size
    
    * feat: utilize image height and width
    
    - get the image height and width
    - normalize dimensions based on a max-width
    - calculate grid-friendly row start/span values based on heights
    
    * feat: implement fallback for masonry layout

--- src/pages/_data/global.js
+++ src/pages/_data/global.js
@@ -1,16 +1,80 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const path = require('path');
+const sizeOf = require('image-size');
+
+const commonAspectRatios = [
+  1 / 1,
+  3 / 2,
+  4 / 3,
+  16 / 9,
+  9 / 16,
+  3 / 4,
+  2 / 3,
+];
+
+const closestAspectRatio = (width, height) => {
+  const rawAspectRatio = width / height;
+  let resultIndex = 0;
+  let difference = Number.POSITIVE_INFINITY;
+
+  commonAspectRatios.forEach((aspectRatio, index) => {
+    const currentDifference = Math.abs(aspectRatio - rawAspectRatio);
+    if (currentDifference < difference) {
+      difference = currentDifference;
+      resultIndex = index;
+    }
+  });
+
+  return commonAspectRatios[resultIndex];
+};
 
 const getImagePaths = () => {
   const directoryPath = path.join(__dirname, '../../public/images/cats');
   const files = fs
     .readdirSync(directoryPath, { withFileTypes: true })
-    .filter((file) => !file.isDirectory())
-    .map((file) => file.name);
+    .filter((file) => !file.isDirectory() && file.name.endsWith('.jpg'))
+    .sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+
+      if (a.name > b.name) {
+        return -1;
+      }
+
+      return 0;
+    })
+    .map((file) => {
+      const STANDARD_WIDTH = 400;
+      const { width, height } = sizeOf(`${directoryPath}/${file.name}`);
+      const aspectRatio = closestAspectRatio(width, height);
+      const adjustedHeight = STANDARD_WIDTH / aspectRatio;
+      return {
+        name: file.name,
+        width,
+        height,
+        aspectRatio,
+        rowSpan: Math.ceil(adjustedHeight / 10),
+      };
+    });
+
+  for (let i = 0; i < files.length; i += 1) {
+    const previousInColumn = files[i - 2];
+    if (!previousInColumn) {
+      files[i].rowStart = 1;
+    } else {
+      files[i].rowStart = previousInColumn.rowStart + previousInColumn.rowSpan + 2;
+    }
+  }
 
   return files;
 };
+
+const picturesOfCats = getImagePaths();
+const catPictureRows = Math.max(
+  ...picturesOfCats.map((picture) => picture.rowStart + picture.rowSpan),
+);
 
 const projects = [
   {
@@ -114,4 +178,5 @@ module.exports = {
   recentProjects: projects.slice(0, 4),
   breakdown: Object.entries(breakdown),
   picturesOfCats: getImagePaths(),
+  catPictureRows,
 };
