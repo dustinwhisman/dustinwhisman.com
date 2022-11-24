0bf1d3d0bcb6e9a13e29fab7642de5e24b5717f9
Dustin Whisman <whisman.dustin@gmail.com>
2022-11-24 08:53:16 -0700

    refactor: replace client-side JS with build-time logic

--- src/js/utilities/set-aria-current.ts
+++
@@ -1,7 +0,0 @@
-export const setAriaCurrent = () => {
-  const { pathname } = window.location;
-  const trimmedPathname = pathname === '/' ? pathname : pathname.replace(/\/+$/, '');
-  const matchingLink = document.querySelector(`nav a[href^="${trimmedPathname}"]`);
-
-  matchingLink?.setAttribute('aria-current', 'page');
-};
