566419b5c0418ee3b6e863efbccb01ea67d43a89
Dustin Whisman <whisman.dustin@gmail.com>
2022-12-03 21:22:46 -0700

    test: set up cypress tests to check each page for accessibility issues

---
+++ cypress.config.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defineConfig } = require('cypress');
+
+const sitemapLocations = async () => {
+  const res = await fetch('https://dustinwhisman.com/sitemap.xml', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+  const xml = await res.text();
+  const locations = [
+    ...xml.matchAll(/<loc>(.|\n)*?<\/loc>/g),
+  ].map(([loc]) => loc.replace('<loc>', '').replace('</loc>', ''));
+  return locations;
+};
+
+module.exports = defineConfig({
+  e2e: {
+    chromeWebSecurity: false,
+    setupNodeEvents: async (on, config) => {
+      on('task', {
+        log(message) {
+          // eslint-disable-next-line no-console
+          console.log(message);
+
+          return null;
+        },
+        table(message) {
+          // eslint-disable-next-line no-console
+          console.table(message);
+
+          return null;
+        },
+      });
+
+      const pages = await sitemapLocations();
+      // eslint-disable-next-line no-param-reassign
+      config.env.pages = pages;
+      return config;
+    },
+  },
+});
