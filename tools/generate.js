8eda99bfa743890ddc06e4ac0201c871da6adb98
dustinwhisman <whisman.dustin@gmail.com>
2023-02-08 11:54:04 -0700

    feat: resolve path to directory where files should be written

--- tools/generate.js
+++ tools/generate.js
@@ -5,11 +5,47 @@ const readline = require('readline/promises').createInterface({
   output: process.stdout,
 });
 
-const collections = [
-  'learning in public',
-  'WAS certification',
-  'eleventy starter template',
-];
+const collections = {
+  LEARNING_IN_PUBLIC: 'learning in public',
+  WAS_CERTIFICATION: 'WAS certification',
+  ELEVENTY_STARTER_TEMPLATE: 'eleventy starter template',
+};
+
+const resolveFilePath = (tags) => {
+  // check for most specific, then less specific
+  if (tags.includes(collections.WAS_CERTIFICATION)) {
+    return path.join(
+      process.cwd(),
+      'src',
+      'pages',
+      'writing',
+      'learning-in-public',
+      'web-accessibility-specialist-certification',
+    );
+  }
+
+  if (tags.includes(collections.LEARNING_IN_PUBLIC)) {
+    return path.join(
+      process.cwd(),
+      'src',
+      'pages',
+      'writing',
+      'learning-in-public',
+    );
+  }
+
+  if (tags.includes(collections.ELEVENTY_STARTER_TEMPLATE)) {
+    return path.join(
+      process.cwd(),
+      'src',
+      'pages',
+      'writing',
+      'eleventy-starter-template',
+    );
+  }
+
+  return path.join(process.cwd(), 'src', 'pages', 'writing');
+};
 
 const generate = async () => {
   const articleName = await readline.question(
@@ -19,16 +55,17 @@ const generate = async () => {
     'What is the description for this article?\n',
   );
   const tags = [];
-  for (let i = 0; i < collections.length; i += 1) {
+  for (const [, collection] of Object.entries(collections)) {
     const includeInTags = await readline.question(
-      `Should this go in the ${collections[i]} collection? y/N?\n`,
+      `Should this go in the ${collection} collection? y/N?\n`,
     );
     if (includeInTags.toLowerCase() === 'y') {
-      tags.push(collections[i]);
+      tags.push(collection);
     }
   }
 
   console.log('Thank you! Creating a new empty post for you now.');
+  console.log(`Writing to ${resolveFilePath(tags)}`);
 
   readline.close();
 };
