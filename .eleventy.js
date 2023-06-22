d9c7b2a2e82a7a58afab382ebffa6729c7382d96
Dustin Whisman <whisman.dustin@gmail.com>
2023-06-22 10:06:23 -0600

    feat: remove Netlify/edge function configs

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,7 @@
-const { EleventyEdgePlugin } = require('@11ty/eleventy');
 const hljs = require('highlight.js');
 const md = require('markdown-it');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyEdgePlugin);
-
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
 
   const markdownOptions = {
