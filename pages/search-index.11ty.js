9e4931fefd674a27a2e8870100c7923cf89bf8b0
dustinwhisman <whisman.dustin@gmail.com>
2024-05-10 13:03:53 -0600

    feat: create a search index

---
+++ pages/search-index.11ty.js
@@ -0,0 +1,22 @@
+class SearchIndex {
+	data() {
+		return {
+			permalink: '/search-index.json',
+			eleventyExcludeFromCollections: true,
+		};
+	}
+
+	render({ collections }) {
+		const searchIndex = collections.all.map((post) => ({
+			title: post.data.title,
+			url: post.url,
+			date: new Date(post.date).toLocaleDateString(undefined, { timeZone: 'UTC' }),
+			text: post.templateContent,
+			description: post.data.description,
+		}));
+
+		return JSON.stringify(searchIndex);
+	}
+}
+
+module.exports = SearchIndex;
